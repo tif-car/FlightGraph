77cb3d86ac2dec008a17c843e55834a6e20580fc
tif-car <68936978+tif-car@users.noreply.github.com>
2024-01-16 22:25:13 -0600

    Add files via upload

---
+++ FlightGraph.cpp
@@ -0,0 +1,33 @@
+//creating a graph to show all flights to and from cities
+//vertex is a city
+//Edge represents plane flight between cities
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Vertex{
+  public:
+    string label;
+    Vertex(const string& vertexLabel){
+      label = vertexLabel;
+    }
+};
+
+class Edge{
+  public:
+    Vertex* fromVertex;
+    Vertex* toVertex;
+    double weight;
+
+    Edge(Vertex* from, Vertex* to, double edgeWeight){
+      fromVertex = from;
+      toVertex = to;
+      weight = edgeWeight;
+    }
+};
+
+int main(){
+  
+}
